c4a5a4dcb0aced3e2ccc194f93dc81bfbedd70b9
ankushDhawan5812 <73323603+ankushDhawan5812@users.noreply.github.com>
2023-08-21 11:00:01 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -68,7 +68,7 @@ html_theme_path = ["_themes", ]
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_logo = "stanford.png"
+html_logo = "stanford-university-stacked.jpeg"
 html_theme_options = {
     'logo_only': True,
     'display_version': False,
