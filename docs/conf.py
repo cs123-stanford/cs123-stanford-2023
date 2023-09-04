552844f517313a94fa391426b76f76a9596e2bfe
ankushDhawan5812 <73323603+ankushDhawan5812@users.noreply.github.com>
2023-09-04 13:51:29 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -67,12 +67,14 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 import sphinx_rtd_theme
 
 html_theme = 'sphinx_rtd_theme'
-html_theme_path = ["_themes", ]
+#html_theme_path = ["_themes", ]
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_css_files = ['css/custom.css']
 html_logo = "stanford-university-stacked.jpeg"
 html_theme_options = {
     'logo_only': True
